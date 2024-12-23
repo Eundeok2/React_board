89495653a6702ab8d8a4692052f8e48a4ab218b2
kkomaeng <ghaehfdl7@naver.com>
2024-12-24 00:32:50 +0900

    🎉 spring boot prj 생성

---
+++ eundeok/src/main/java/com/eundeok/EundeokApplication.java
@@ -0,0 +1,13 @@
+package com.eundeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EundeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EundeokApplication.class, args);
+	}
+
+}
---
+++ eundeok/src/test/java/com/eundeok/EundeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eundeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EundeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
