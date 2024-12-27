11c0f8f03185131ce29879014444798bcd9fa697
강은수 <kes2450@naver.com>
2024-12-28 03:00:19 +0900

    :sparkles: useState 설정해 App.js에 상태 관리 추가

--- board_front/src/App.js
+++ board_front/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import ArticleList from "./components/ArticleList";
 import ArticleCreate from "./components/ArticleCreate";
@@ -6,7 +6,8 @@ import ArticleDetail from "./components/ArticleDetail";
 import ArticleUpdate from "./components/ArticleUpdate";
 
 const App = () => {
-
+  // 게시글 데이터 상태 관리 (React state)
+  const [articles, setArticles] = useState([]);
 
 
   return(
@@ -15,15 +16,13 @@ const App = () => {
         <Link to="/article-list">글 목록</Link>
       </nav>
       <Routes>
-        <Route path="/article-list" element={<ArticleList />} />
-        <Route path="/article-create" element={<ArticleCreate />} />
-        <Route path="/article-detail/:id" element={<ArticleDetail />} />
-        <Route path="/article-update/:id" element={<ArticleUpdate />} />
+        <Route path="/article-list" element={<ArticleList articles={articles} />} />
+        <Route path="/article-create" element={<ArticleCreate setArticles={setArticles} articles={articles}/>} />
+        <Route path="/article-detail/:id" element={<ArticleDetail articles={articles} />} />
+        <Route path="/article-update/:id" element={<ArticleUpdate articles={articles} setArticles={setArticles} />} />
       </Routes>
     </Router>
-
   )
-  
 };
 
 
