aa77d9d6dcaca33a56a9f9601277dbb75c692ea5
강은수 <kes2450@naver.com>
2024-12-28 02:54:32 +0900

    :sparkles: App.js에  글상세, 글수정 링크 추가

--- board_front/src/App.js
+++ board_front/src/App.js
@@ -2,18 +2,29 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import ArticleList from "./components/ArticleList";
 import ArticleCreate from "./components/ArticleCreate";
+import ArticleDetail from "./components/ArticleDetail";
+import ArticleUpdate from "./components/ArticleUpdate";
 
-const App = () => (
-  <Router>
-    <nav>
-      <Link to="/article-list">글 목록</Link>
-    </nav>
-    <Routes>
-      <Route path="/article-list" element={<ArticleList />} />
-      <Route path="/article-create" element={<ArticleCreate />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+
+
+
+  return(
+    <Router> 
+      <nav>
+        <Link to="/article-list">글 목록</Link>
+      </nav>
+      <Routes>
+        <Route path="/article-list" element={<ArticleList />} />
+        <Route path="/article-create" element={<ArticleCreate />} />
+        <Route path="/article-detail/:id" element={<ArticleDetail />} />
+        <Route path="/article-update/:id" element={<ArticleUpdate />} />
+      </Routes>
+    </Router>
+
+  )
+  
+};
 
 
 export default App;
