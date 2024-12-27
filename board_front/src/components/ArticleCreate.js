ecff03823adbebdccaa15d03e75f5abf775cfbd4
강은수 <kes2450@naver.com>
2024-12-28 03:35:30 +0900

    :sparkles: 게시글 작성 로직 구현(Dto에 맞춰서)

--- board_front/src/components/ArticleCreate.js
+++ board_front/src/components/ArticleCreate.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const ArticleCreate = () => {
+const ArticleCreate = ({ articles, setArticles }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState(""); // 작성자 추가
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newArticle = {
+      postId: Date.now(),     // 게시글 번호 (고유 id)
+      title: title,
+      content: content,
+      author: author,
+      viewContent: 0,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+
+    setArticles([...articles, newArticle]); // 기존 데이터에 추가
+    navigate(`/article-detail/${newArticle.postId}`)   // 작성된 글의 상세 페이지로 이동
+  }  
+
   return(
     <div>
       <h1>글 작성 페이지</h1>
+      <form onSubmit={handleSubmit}>
+        <label>작성자: </label>
+        <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <br />
+        <label>제목: </label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <br />
+        <label>내용: </label>
+        <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+        <br />
+        <button type="submit">글 작성</button>
+      </form>
     </div>
   );
 };
