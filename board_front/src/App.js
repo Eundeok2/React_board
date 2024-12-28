080714f3e097eeaeab9c7b8ad64cae2273214a3c
kkomaeng <ghaehfdl7@naver.com>
2024-12-29 01:45:06 +0900

    ♻️ 컴포넌트에서 데이터 불러오기

--- board_front/src/App.js
+++ board_front/src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import axios from "axios";
 import ArticleList from "./components/ArticleList";
 import ArticleCreate from "./components/ArticleCreate";
 import ArticleDetail from "./components/ArticleDetail";
@@ -8,15 +7,6 @@ import ArticleUpdate from "./components/ArticleUpdate";
 import styles from './App.module.css';
 
 const App = () => {
-  // 게시글 데이터 상태 관리 (React state)
-  const [articles, setArticles] = useState([]);
-
-  // 데이터 가져오기
-  useEffect(() => {
-    axios.get("http://localhost:8080/react-board/articles")
-      .then(res => setArticles(res.data))
-      .catch(err => console.log(err))
-  },[])
 
   return(
     <div className={styles.container}>
@@ -25,10 +15,12 @@ const App = () => {
           <Link to="/article-list">글 목록</Link>
         </nav>
         <Routes>
-          <Route path="/article-list" element={<ArticleList articles={articles} />} />
-          <Route path="/article-create" element={<ArticleCreate setArticles={setArticles} articles={articles}/>} />
+          <Route path="/article-list" element={<ArticleList />} />
+          {/* setArticles={setArticles} articles={articles} */}
+          <Route path="/article-create" element={<ArticleCreate />} />
           <Route path="/article-detail/:id" element={<ArticleDetail />} />
-          <Route path="/article-update/:id" element={<ArticleUpdate articles={articles} setArticles={setArticles} />} />
+          {/* articles={articles} setArticles={setArticles} */}
+          <Route path="/article-update/:id" element={<ArticleUpdate />} />
         </Routes>
       </Router>
     </div>
--- board_front/src/components/ArticleList.jsx
+++ board_front/src/components/ArticleList.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Link } from "react-router-dom"; 
 import styles from "./styles/ArticleList.module.css"
+import axios from "axios";
 
-const ArticleList = ({ articles }) => {
+const ArticleList = () => {
+  // 게시글 데이터 상태 관리 (React state)
+  const [articles, setArticles] = useState([]);
+
+  // 데이터 가져오기
+  useEffect(() => {
+    axios.get("http://localhost:8080/react-board/articles")
+      .then(res => setArticles(res.data))
+      .catch(err => console.log(err))
+  },[])
+
   return(
     <div className={styles.container}>
       <h1 className={styles.listTitle}>전체 게시글 목록</h1>
